36ce487a7a6b7f9f198f96d8bd748367af3ec8e7
zismano <zismanofir@gmail.com>
2017-12-14 10:22:04 -0800

    Youtube requests work and auto render with results. Specs still not passing.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,44 +2,48 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      video: window.exampleVideoData[0]
-   };
- }
+      video: window.exampleVideoData[0],
+      videoCollection: window.exampleVideoData
+    };
+  }
 
   getVideos (query) {
+    var self = this;
     $.ajax({
       url: 'https://www.googleapis.com/youtube/v3/search',
       type: 'GET',
-      data: {'maxResults': '5',
-      key: window.YOUTUBE_API_KEY,
-      part: 'snippet',
-      q: query,
-      type: ''},
-      
+      data: {
+        'maxResults': '5',
+        'key': window.YOUTUBE_API_KEY, 
+        'part': 'snippet',
+        'q': query,
+        'type': ''
+      },
       success: function(data) {
-        console.log('success!', data)
+        console.log('success!', data);
+        self.setState({videoCollection: data.items});
       },
       error: function(data) {
-        console.error("Failed", data);
+        console.error('Failed', data);
       }   
     });
   }  
  
 
- handlerSelectVideo(video) {
-   console.log(video);
-   this.setState({
-     video: video
-   });
- }
+  handlerSelectVideo(video) {
+    console.log(video);
+    this.setState({
+      video: video
+    });
+  }
 
- render() {
+  render() {
  
-   return (
+    return (
      <div>
        <nav className="navbar">
          <div className="col-md-6 offset-md-3">
-           <Search search={this.getVideos} />
+           <Search search={this.getVideos.bind(this)} />
          </div>
        </nav>
        <div className="row">
@@ -47,12 +51,12 @@ class App extends React.Component {
            <VideoPlayer video={this.state.video}/>
          </div>
          <div className="col-md-5">
-           <VideoList videos={window.exampleVideoData} handler={this.handlerSelectVideo.bind(this)}/>
+           <VideoList videos={this.state.videoCollection} handler={this.handlerSelectVideo.bind(this)}/>
          </div>
        </div>
      </div>
-   );
- }
+    );
+  }
 }
 
 
