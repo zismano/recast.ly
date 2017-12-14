006b12a5d65e9f30aeec2a5e91f8162ac9d6e412
zismano <zismanofir@gmail.com>
2017-12-13 18:26:56 -0800

    Modify App to class

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,29 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <Search /> 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <Search /> 
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <VideoPlayer video={window.exampleVideoData[0]}/>
+          </div>
+          <div className="col-md-5">
+            <VideoList videos={window.exampleVideoData} />
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <VideoPlayer video={window.exampleVideoData[0]}/>
-      </div>
-      <div className="col-md-5">
-        <VideoList videos={window.exampleVideoData} />
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
-//window.App = App;
+window.App = App;
